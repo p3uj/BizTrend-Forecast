1ff86ec80949abd8ebe7553ec1d2cfa29b8ba251
Bengie B. Villesco <bvillesco@gmail.com>
2025-04-09 23:40:37 +0800

    Added route for log in page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LogIn";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
