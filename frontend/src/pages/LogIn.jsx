26fc93c9d5a460376c5254fe99c6ae4dcf1e24af
Bengie B. Villesco <bvillesco@gmail.com>
2025-04-26 11:21:55 +0800

    Update: - Implement the validation for required fields. - Display error for invalid email format. - Make the log in button clickable only when the required fields are valid.

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -2,8 +2,26 @@ import "../css/custom-colors.css";
 import "../css/LogIn.css";
 import logInImage from "../assets/img/login.png";
 import logo from "../assets/img/logo.png";
+import { useState } from "react";
 
 function LogIn() {
+  const [requiredFieldsData, setRequiredFieldsData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleValueFields = (event) => {
+    const { name, value } = event.target; // Destructures name and value from the input event.
+    setRequiredFieldsData((prevData) => ({
+      ...prevData, // Keep all existing value
+      [name]: value, // Update the field that changed.
+    }));
+  };
+
+  // Validate the required input fields.
+  const isRequiredFieldValid =
+    requiredFieldsData.email.includes("@") && requiredFieldsData.password != "";
+
   return (
     <main className="login-page">
       <section className="left-panel">
@@ -13,28 +31,37 @@ function LogIn() {
         <img src={logo} alt="logo" />
         <form action="" method="post">
           <fieldset>
-            <label>Email:</label>
+            <label htmlFor="email">Email:</label>
+
+            {/* Display error for invalid email format if the email does not have '@' */}
+            {!requiredFieldsData.email.includes("@") &&
+              requiredFieldsData.email != "" && <span>Invalid format.</span>}
+
             <input
               type="email"
               name="email"
               id="email"
               required
               placeholder="Enter email"
+              onChange={handleValueFields}
             />
           </fieldset>
 
           <fieldset>
-            <label>Password:</label>
+            <label htmlFor="password">Password:</label>
             <input
               type="password"
               name="password"
               id="password"
               required
               placeholder="Enter password"
+              onChange={handleValueFields}
             />
           </fieldset>
 
-          <button type="submit">Log In</button>
+          <button type="submit" disabled={!isRequiredFieldValid}>
+            Log In
+          </button>
         </form>
         <a href="#">Forgot Password?</a>
       </section>
