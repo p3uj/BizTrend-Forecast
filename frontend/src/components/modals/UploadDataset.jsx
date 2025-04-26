0617f3ff9f88ec7ccfa9f12db44cd77ed755c036
Bengie B. Villesco <bvillesco@gmail.com>
2025-04-26 16:57:05 +0800

    Update: Finish the UI for upload dataset modal.

--- frontend/src/components/modals/UploadDataset.jsx
+++ frontend/src/components/modals/UploadDataset.jsx
@@ -2,9 +2,30 @@ import "../../css/UploadDataset.css";
 import CloseIconBlack from "../../assets/icons/close-black.svg";
 import CloseIconWhite from "../../assets/icons/close-white.svg";
 import { useState } from "react";
+import UploadIcon from "../../assets/icons/upload.svg";
 
 export default function UploadDataset({ showModal }) {
   const [isCloseBtnHover, setCloseBthHover] = useState(false);
+  const [fileSelected, setFileSelected] = useState(null);
+
+  console.log("file selected: ", fileSelected);
+
+  const handleFileUpload = (event) => {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      setFileSelected(file);
+    } else {
+      setFileSelected(null);
+    }
+  };
+
+  const handleRemoveFile = (e) => {
+    e.preventDefault(); // Prevent default behavior.
+    e.stopPropagation(); // Stop propagation to prevent label click.
+
+    document.getElementById("file").value = ""; // Reset the value in the input file.
+    setFileSelected(null); // Clear value of the fileSelected state.
+  };
 
   return (
     <div className="upload-dataset-modal">
@@ -20,12 +41,45 @@ export default function UploadDataset({ showModal }) {
             alt="close-icon"
           />
         </button>
-        <h1>Notes here!</h1>
+        <h2>Upload Dataset</h2>
+        <p className="reminder">
+          Please make sure the dataset is complete to avoid inaccurate result.
+        </p>
         <section>
-          <input type="file" name="file" id="file" />
-          <label htmlFor="file">Choose File</label>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            accept=".xls, .xlsx"
+            style={{ display: "none" }}
+            onChange={handleFileUpload}
+          />
+          <label htmlFor="file">
+            <img src={UploadIcon} alt="upload-icon" className="upload-icon" />
+            <p>
+              <span>Click here</span> to upload your file.
+            </p>
+            <p className="supported-format">Supported Format: xls, xlsx</p>
+
+            {fileSelected && (
+              <div className="selected-file">
+                <p>{fileSelected.name}</p>
+                <img
+                  src={CloseIconBlack}
+                  alt="remove"
+                  className="remove-icon"
+                  onClick={handleRemoveFile}
+                />
+              </div>
+            )}
+          </label>
         </section>
-        <button>Make Prediction</button>
+        <button
+          disabled={fileSelected === null}
+          className="make-prediction-btn"
+        >
+          Make Prediction
+        </button>
       </main>
     </div>
   );
