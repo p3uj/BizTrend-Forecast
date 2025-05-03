bca72cbfc750f1fd7ab46a82833fa926b5be71b7
Bengie B. Villesco <bvillesco@gmail.com>
2025-05-03 22:07:19 +0800

    Added route for registration page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LogIn";
 import HomePage from "./pages/Home";
+import RegisterPage from "./pages/Register";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
