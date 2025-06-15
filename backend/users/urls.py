c3ddb7639684138d0042fbb92f29167670eff4a7
Bengie B. Villesco <bvillesco@gmail.com>
2025-06-15 22:28:39 +0800

    Backend Update: add endpoint for dataset

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,5 +6,6 @@ from .views import *
 router = DefaultRouter()
 router.register('register', RegisterViewset, basename='register')
 router.register('users', UserViewset, basename='users')
+router.register('dataset', DatasetViewset, basename='dataset')
 router.register('dataset_validation', DataValidationViewset, basename='dataset_validation')
 urlpatterns = router.urls
