1e10bb137a9fb5aca4f81752aa057fdb134f4af2
Bengie B. Villesco <bvillesco@gmail.com>
2025-06-16 16:41:25 +0800

    Backend: Register Dataset, PredictionResult, and Trend models in the admin interface

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,3 +4,6 @@ from .models import *
 # Register your models here.
 
 admin.site.register(CustomUser)
+admin.site.register(Dataset)
+admin.site.register(PredictionResult)
+admin.site.register(Trend)
