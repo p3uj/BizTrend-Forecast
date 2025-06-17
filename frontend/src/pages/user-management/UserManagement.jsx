0752ac07de4980223f5bd1e1f6d8f94783b941c4
Bengie B. Villesco <bvillesco@gmail.com>
2025-06-18 03:04:51 +0800

    UI: Add user management page (not yet finish).

---
+++ frontend/src/pages/user-management/UserManagement.jsx
@@ -0,0 +1,173 @@
+import { useState } from "react";
+import Navbar from "../../components/NavBar";
+import "../../css/UserManagement.css";
+import SampleProfile from "../../assets/img/do.png";
+import RegisterUser from "../../components/modals/RegisterUser";
+
+export default function UserManagement() {
+  const [activeTab, setActiveTab] = useState("All");
+  const [isAddUser, setAddUser] = useState(false);
+
+  return (
+    <>
+      {isAddUser && (
+        <RegisterUser showRegisterUserModal={() => setAddUser(false)} />
+      )}
+
+      <nav>
+        <Navbar />
+      </nav>
+      <main className="user-management">
+        <section>
+          <div className="title-page">
+            <h1>User Management</h1>
+            <button onClick={() => setAddUser(true)}>Add User</button>
+          </div>
+        </section>
+        <section>
+          <div className="table-header">
+            <h2>All Users (8)</h2>
+            <ul>
+              <li
+                className={activeTab === "All" ? "active" : ""}
+                onClick={() => setActiveTab("All")}
+              >
+                All
+              </li>
+              <li
+                className={activeTab === "Active" ? "active" : ""}
+                onClick={() => setActiveTab("Active")}
+              >
+                Active
+              </li>
+              <li
+                className={activeTab === "Inactive" ? "active" : ""}
+                onClick={() => setActiveTab("Inactive")}
+              >
+                Inactive
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="grid-container">
+          <article className="grid-item-1">
+            <img src={SampleProfile} alt="Profile" />
+            <h2>Mary Grace Piattos</h2>
+            <section className="status-and-role">
+              <span className="status" data-status={"active"}>
+                <div className="dot"></div>
+                <p>Active</p>
+              </span>
+              <span className="role" data-role={"admin"}>
+                <div className="dot"></div>
+                <p>Admin</p>
+              </span>
+            </section>
+            <button>Deactivate Account</button>
+          </article>
+          <article className="grid-item-2">
+            <img src={SampleProfile} alt="Profile" />
+            <h2>Jay Kamote</h2>
+            <section className="status-and-role">
+              <span className="status" data-status={"inactive"}>
+                <div className="dot"></div>
+                <p>Inactive</p>
+              </span>
+            </section>
+            <button>Activate Account</button>
+          </article>
+          <article className="grid-item-3">
+            <img src={SampleProfile} alt="Profile" />
+            <h2>Mary Grace Piattos</h2>
+            <section className="status-and-role">
+              <span className="status" data-status={"active"}>
+                <div className="dot"></div>
+                <p>Active</p>
+              </span>
+              <span className="role" data-role={"admin"}>
+                <div className="dot"></div>
+                <p>Admin</p>
+              </span>
+            </section>
+            <button>Make Inactive</button>
+          </article>
+          <article className="grid-item-4">
+            <img src={SampleProfile} alt="Profile" />
+            <h2>Mary Grace Piattos</h2>
+            <section className="status-and-role">
+              <span className="status" data-status={"active"}>
+                <div className="dot"></div>
+                <p>Active</p>
+              </span>
+              <span className="role" data-role={"admin"}>
+                <div className="dot"></div>
+                <p>Admin</p>
+              </span>
+            </section>
+            <button>Make Inactive</button>
+          </article>
+          <article className="grid-item-5">
+            <img src={SampleProfile} alt="Profile" />
+            <h2>Mary Grace Piattos</h2>
+            <section className="status-and-role">
+              <span className="status" data-status={"active"}>
+                <div className="dot"></div>
+                <p>Active</p>
+              </span>
+              <span className="role" data-role={"admin"}>
+                <div className="dot"></div>
+                <p>Admin</p>
+              </span>
+            </section>
+            <button>Make Inactive</button>
+          </article>
+          <article className="grid-item-6">
+            <img src={SampleProfile} alt="Profile" />
+            <h2>Mary Grace Piattos</h2>
+            <section className="status-and-role">
+              <span className="status" data-status={"active"}>
+                <div className="dot"></div>
+                <p>Active</p>
+              </span>
+              <span className="role" data-role={"admin"}>
+                <div className="dot"></div>
+                <p>Admin</p>
+              </span>
+            </section>
+            <button>Make Inactive</button>
+          </article>
+          <article className="grid-item-7">
+            <img src={SampleProfile} alt="Profile" />
+            <h2>Mary Grace Piattos</h2>
+            <section className="status-and-role">
+              <span className="status" data-status={"active"}>
+                <div className="dot"></div>
+                <p>Active</p>
+              </span>
+              <span className="role" data-role={"admin"}>
+                <div className="dot"></div>
+                <p>Admin</p>
+              </span>
+            </section>
+            <button>Make Inactive</button>
+          </article>
+          <article className="grid-item-8">
+            <img src={SampleProfile} alt="Profile" />
+            <h2>Mary Grace Piattos</h2>
+            <section className="status-and-role">
+              <span className="status" data-status={"active"}>
+                <div className="dot"></div>
+                <p>Active</p>
+              </span>
+              <span className="role" data-role={"admin"}>
+                <div className="dot"></div>
+                <p>Admin</p>
+              </span>
+            </section>
+            <button>Make Inactive</button>
+          </article>
+        </section>
+      </main>
+    </>
+  );
+}
