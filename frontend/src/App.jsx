9d7f2fbccf84d30f556fda5b38a2ee98e567b709
Bengie B. Villesco <bvillesco@gmail.com>
2025-06-18 03:08:15 +0800

    UI: Add route for User Management page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/LogIn";
 import HomePage from "./pages/Home";
 import RegisterPage from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoutes";
+import UserManagement from "./pages/user-management/UserManagement";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/user-management" element={<UserManagement />} />
 
         <Route element={<ProtectedRoute />}>
           {/* <Route path="/home" element={<HomePage />} /> */}
