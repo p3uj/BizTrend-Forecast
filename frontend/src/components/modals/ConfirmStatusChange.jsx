d97aabd8eb4add60cff9f99c5b7291d192f24911
Bengie B. Villesco <bvillesco@gmail.com>
2025-06-18 03:09:12 +0800

    UI: Add Confirm Status Change modal component (not yet finish)

---
+++ frontend/src/components/modals/ConfirmStatusChange.jsx
@@ -0,0 +1,8 @@
+export default function ConfirmStatusChange() {
+  return (
+    <section className="confirm-status-change-modal">
+      <section className="overlay"></section>
+      <section className="content"></section>
+    </section>
+  );
+}
