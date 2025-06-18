6c89afcf45eab53aa0bfa56b86692a05166227aa
Bengie B. Villesco <bvillesco@gmail.com>
2025-06-18 19:00:12 +0800

    UI: Add RippleLoading component for loading animations

---
+++ frontend/src/components/modals/loading/RippleLoading.jsx
@@ -0,0 +1,10 @@
+export default function RippleLoading() {
+  return (
+    <div className="loading-ripple">
+      <div className="circle-1"></div>
+      <div className="circle-2"></div>
+      <div className="circle-3"></div>
+      <div className="circle-4"></div>
+    </div>
+  );
+}
