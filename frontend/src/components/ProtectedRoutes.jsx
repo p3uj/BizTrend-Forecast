e8ff1cb57a74e75ede394c5f9da29b8ed34282c8
Bengie B. Villesco <bvillesco@gmail.com>
2025-06-19 12:13:16 +0800

    Access Control: Enhance ProtectedRoute component to enforce access control for non-superuser roles

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -3,5 +3,16 @@ import { Outlet, Navigate } from "react-router-dom";
 export default function ProtectedRoute() {
   const token = localStorage.getItem("access_token");
 
+  if (!token) {
+    return <Navigate to="/" />;
+  }
+
+  if (sessionStorage.getItem("current_user")) {
+    const currentUser = JSON.parse(sessionStorage.getItem("current_user"));
+    if (!currentUser.is_superuser) {
+      return <Navigate to="/" />;
+    }
+  }
+
   return token ? <Outlet /> : <Navigate to="/" />;
 }
